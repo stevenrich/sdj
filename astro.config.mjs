9114cf384350618083e53342f0ecf802732488ca
Steven Rich <databae@databae.local>
2025-05-18 22:03:41 -0400

    pushing repo

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+  },
+
+  adapter: netlify(),
+})
